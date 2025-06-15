9ba43942e0bf8444ad409a215e98860096fbbc46
Nabila <ASUS@DESKTOP-QRIVNUR>
2025-06-15 21:47:45 +0700

    Add Negative Testcase Create testcase validation required, testcase invalid email, testcase invalid mobile number

---
+++ Object Repository/Object Practice Form/Form.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Form</name>
+   <tag></tag>
+   <elementGuidId>c01b3632-9505-4147-8074-e73f38fc5916</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;userForm&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Scripts/Negative Case/Testcase Invalid Email/Script1749998275942.groovy
@@ -0,0 +1,40 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.callTestCase(findTestCase('General/Go to URL'), [:], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.setText(findTestObject('Object Practice Form/Input Text', [('id') : 'firstName']), 'Laundry')
+
+WebUI.setText(findTestObject('Object Practice Form/Input Text', [('id') : 'lastName']), 'Simulator')
+
+WebUI.setText(findTestObject('Object Practice Form/Input Text', [('id') : 'userEmail']), 'anomalilaundry')
+
+'Select Male'
+WebUI.click(findTestObject('Object Practice Form/Gender Options', [('genderOption') : 'Male']))
+
+WebUI.setText(findTestObject('Object Practice Form/Input Text', [('id') : 'userNumber']), '1234567890')
+
+WebUI.scrollToElement(findTestObject('Object Practice Form/Input Text', [('id') : 'currentAddress']), 0)
+
+WebUI.click(findTestObject('Object Practice Form/Button/Button Submit'))
+
+WebUI.verifyElementAttributeValue(findTestObject('Object Practice Form/Form'), 'class', 'was-validated', 0)
+
+WebUI.scrollToElement(findTestObject('Object Practice Form/Title Page and Modal/Title Form'), 0)
+
